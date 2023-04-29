ac9504b668d575fcf297a1029d39a9871f5e8de9
kiwilayder <kiwilay@mail.ru>
2023-04-29 23:11:51 +0400

    .

--- src/actions/localUserTrening.js
+++ src/actions/localUserTrening.js
@@ -2,29 +2,26 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { asyncLocalStorage } from "../helpers/asyncLocalStorage";
 import { updateDayProgress, updateMaxValue } from "../store/userTreningSlice";
 
-export const setLocalMaxValue = createAsyncThunk(
-   "user/setLocalMaxValue",
-   async (treningData, { dispatch }) => {
-      const userDataTrening = { ...treningData, dayProgress: 0 };
-      await asyncLocalStorage.setItem(`${userTreningData.type}`, userDataTrening);
-      dispatch(updateMaxValue({ ...userDataTrening }));
-   }
-);
+export const setLocalMaxValue = createAsyncThunk("user/setLocalUser", async (treningData, { dispatch }) => {
+   const userTreningData = { ...treningData, dayProgress: 0 };
+   await asyncLocalStorage.setItem(`${userTreningData.type}`, userTreningData);
+   dispatch(updateMaxValue({ ...userTreningData }));
+});
 
 export const setLocalDayProgress = createAsyncThunk(
    "user/setLocalDayProgress",
-   async (treningData, { dispatch }) => {
-      const userDataTrening = await asyncLocalStorage.getItem(`${treningData.type}`);
+   async (userTreningData, { dispatch }) => {
+      const dataTrening = await asyncLocalStorage.getItem(`${userTreningData.type}`);
 
       await asyncLocalStorage.setItem(`${userTreningData.type}`, {
-         ...userDataTrening,
-         dayProgress: ++treningData.dayProgress,
+         ...dataTrening,
+         dayProgress: ++dataTrening.dayProgress,
       });
       dispatch(updateDayProgress({ ...userTreningData }));
    }
 );
 
 export const getDataTrening = createAsyncThunk("user/getDataTrening", async (typeTrening, { dispatch }) => {
-   const userDataTrening = await asyncLocalStorage.getItem(typeTrening);
-   dispatch(updateMaxValue({ ...userDataTrening }));
+   const userTreningData = await asyncLocalStorage.getItem(typeTrening);
+   dispatch(updateMaxValue({ ...userTreningData }));
 });
